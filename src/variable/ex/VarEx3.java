8385bc56dd4ae4d8666dd52d20f1867a13ba4fec
SeoYeKyung <seodbsk78@gmail.com>
2024-08-30 17:02:01 +0900

    add 예제 문제

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L; //long은 뒤에 L을 붙이는 것을 기억!
+
+        boolean booleanVar=true;
+
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+    }
+
+}
